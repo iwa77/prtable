d71b8b49a11cf3dc1739f7d795958d03c119d475
iwa77 <iwa777777@gmail.com>
2017-06-07 00:28:10 +0900

    change

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,4 +2,11 @@ class StoriesController < ApplicationController
 
   def index
   end
+
+  def new
+  end
+
+  def create
+    Story.create(name: params[:name], image: params[:image], text: params[:text])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   devise_for :companies
 
   get 'stories' => 'stories#index'
-
+  get 'stories/new'  =>  'stories#new' #ストーリー投稿画面
+  post 'tweets'      =>  'tweets#create' #ストーリー投稿機能
 
   root 'stories#index'
+
+  
+  
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
